acf73cc64b9764a62b084b386ff32d6639366866
ferbarragan <ferbarragan.developer@gmail.com>
2016-07-19 19:22:50 -0500

    Initial Commit

---
+++ WebServicesExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebServicesExample
+//
+//  Created by Christian Barragan on 19/07/16.
+//  Copyright © 2016 Christian Barragan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebServicesExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebServicesExample
+//
+//  Created by Christian Barragan on 19/07/16.
+//  Copyright © 2016 Christian Barragan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
